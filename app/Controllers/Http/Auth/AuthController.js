74cc023f56c19fe51fe10eda49dd52f43ef67b07
hemerson-git <hemersonsimoni@gmail.com>
2020-10-07 02:04:10 -0300

    User Register Method

--- app/Controllers/Http/Auth/AuthController.js
+++ app/Controllers/Http/Auth/AuthController.js
@@ -1,12 +1,34 @@
 'use strict';
 
+const Database = use('Database');
+const User = use('App/Models/User');
+const Role = use('Role');
+
 class AuthController {
   async register({ request, response }) {
+    const trx = await Database.beginTransection();
 
+    try {
+      const { name, surname, email, password } = request.all();
+
+      const user = await User.create({ name, surname, email, password }, trx);
+      
+      const userRole = await Role.findBy('slug', 'client');
+      await user.roles().attach([userRole.id], null, trx);
+
+      await trx.commit();
+
+      return response.status(201).send({ data: user });
+    } catch (error) {
+      await trx.rollback();
+      return response.status(400).send({ 
+        message: 'Erro ao realizar cadastro!',
+      });
+    }
   }
 
   async login({ request, response, auth}) {
-
+    
   }
 
   async refresh({ request, response, auth }) {
