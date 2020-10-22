6db677256a0fd3ad374ba8a2fde702a128114523
hemerson-git <hemersonsimoni@gmail.com>
2020-10-22 00:09:29 -0300

    Criação dos Helpers

---
+++ app/Helpers/index.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const crypto = use('crypto');
+const Helpers = use('Helpers');
+
+/**
+ * Generate ramdom strings
+ * 
+ * @param { int } length - O tamanho da string desejada
+ * @return { string } - A string gerada
+ */
+
+const str_random = async (length = 40) => {
+  let str = '';
+  let len = str.length;
+
+  if(len < length) {
+    let size = length - len;
+    let bytes = await crypto.randomBytes(size);
+    let buffer = new Buffer.from(bytes);
+    str += buffer.toString('base64').replace('/[a-zA-Z0-0]/g', '').substr(0, size);
+  }
+
+  return str;
+};
+
+module.exports = {
+  str_random;
+};
