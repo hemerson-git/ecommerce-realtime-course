a776436f6a006fa65fb28f85eae03ca1bf2bd925
hemerson-git <hemersonsimoni@gmail.com>
2020-11-01 00:39:21 -0300

    Partial OrderController and OrderService

--- app/Controllers/Http/Admin/OrderController.js
+++ app/Controllers/Http/Admin/OrderController.js
@@ -4,6 +4,9 @@
 /** @typedef {import('@adonisjs/framework/src/Response')} Response */
 /** @typedef {import('@adonisjs/framework/src/View')} View */
 
+const Order = use('App/Models/Order'); 
+const Database = use('Database');
+
 /**
  * Resourceful controller for interacting with orders
  */
@@ -15,9 +18,25 @@ class OrderController {
    * @param {object} ctx
    * @param {Request} ctx.request
    * @param {Response} ctx.response
+   * @param {Object} ctx.pagination
    * @param {View} ctx.view
    */
-  async index ({ request, response, view }) {
+  async index ({ request, response, pagination }) {
+    const { status, id } = request.only(['status, only']);
+    const query = Order.query();
+
+    if (status && id) {
+      query.where('status', status);
+      query.orWhere('id', 'LIKE', `%${id}%`);
+    } else if (id) {
+      query.where('id', 'LIKE', `%${id}%`);
+    } else if (status) {
+      query.where('status', status);
+    } 
+
+    const orders = await query.paginate(pagination.page, pagination.limit);
+
+    return response.send(orders);
   }
 
   /**
@@ -29,6 +48,7 @@ class OrderController {
    * @param {Response} ctx.response
    */
   async store ({ request, response }) {
+    
   }
 
   /**
@@ -38,9 +58,10 @@ class OrderController {
    * @param {object} ctx
    * @param {Request} ctx.request
    * @param {Response} ctx.response
-   * @param {View} ctx.view
    */
-  async show ({ params, request, response, view }) {
+  async show ({ params: { id }, request, response }) {
+    const order = await Order.findOrFail(id);
+    return response.send(order);
   }
 
   /**
@@ -62,7 +83,24 @@ class OrderController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async destroy ({ params, request, response }) {
+  async destroy ({ params: { id }, request, response }) {
+    const order = await Order.findOrFail(id);
+    const trx = await Database.beginTransaction();
+
+    try {
+      await order.items().delete(trx);
+      await order.coupons().delete(trx);
+      await order.delete();
+
+      await trx.commit();
+      return response.status(204).send();
+    } catch (error) {
+      await trx.rollback();
+      return response.status(400).send({
+        message: 'Erro ao deletar essa compra',
+      });
+    }
+
   }
 }
 
